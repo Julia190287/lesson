bc1eb023dfb56929bf0af82991b64cef007e2fac
Julia190287 <mus19.02.87@mail.ru>
2024-01-13 16:35:31 +0300

    homework number 2

--- 1.js
+++ 1.js
@@ -1,4 +1,6 @@
 console.log("My name is Julia");
 console.log(2+2);
-console.log('hello world')
-alert('blabla')
+console.log('hello world');
+console.log(2);
+console.log("Добро"+"пожаловать"+"на курс"+"Юлия");
+// alert('13 января 2024')
