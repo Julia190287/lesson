5dba0babcecbb394d3567e28a520bc57be3e5bd7
Julia190287 <mus19.02.87@mail.ru>
2024-01-14 13:12:43 +0300

    homework №3

--- 1.js
+++ 1.js
@@ -1,6 +1,12 @@
-console.log("My name is Julia");
-console.log(2+2);
-console.log('hello world');
-console.log(2);
-console.log("Добро"+"пожаловать"+"на курс"+"Юлия");
+//console.log("My name is Julia");
+//console.log(2+2);
+//console.log('hello world');
+//console.log(2);
+//console.log("Добро"+"пожаловать"+"на курс"+"Юлия");
 // alert('13 января 2024')
+let number = prompt ("Выберите число", "");
+    if (number < 100 ) {
+        console.log("меньше ста");
+    } else if (number > 100){
+        alert("Больше ста");       
+    }
