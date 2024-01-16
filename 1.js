25e2b10225a49a582cb586280c5cc99f18fdd7f9
Julia190287 <mus19.02.87@mail.ru>
2024-01-16 23:05:08 +0300

    homework №4

--- 1.js
+++ 1.js
@@ -4,9 +4,26 @@
 //console.log(2);
 //console.log("Добро"+"пожаловать"+"на курс"+"Юлия");
 // alert('13 января 2024')
-let number = prompt ("Выберите число", "");
-    if (number < 100 ) {
-        console.log("меньше ста");
-    } else if (number > 100){
-        alert("Больше ста");       
-    }
+//let number = prompt ("Выберите число", "");
+   //if (number < 100 ) {
+   //     console.log("меньше ста");
+  //  } else if (number > 100){
+    //    alert("Больше ста");       
+   // }
+  
+  let num = prompt ("сумма a+b")
+  if(num > 15){
+   console.log('ДА');
+   }else if(num <= 15){
+  console.log('НЕТ');
+  } // задание 1
+  let a = 10
+  var e = "зима"
+  if(e === a){
+  console.log(true);
+  }
+  else{
+  console.log(false);
+  
+  } // задание 2
+  
