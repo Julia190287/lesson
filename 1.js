10ffc6760edbd12ec5cb600e0f3d852cc7537652
Julia190287 <mus19.02.87@mail.ru>
2024-01-17 21:12:36 +0300

    homework 5

--- 1.js
+++ 1.js
@@ -11,19 +11,29 @@
     //    alert("Больше ста");       
    // }
   
-  let num = prompt ("сумма a+b")
-  if(num > 15){
-   console.log('ДА');
-   }else if(num <= 15){
-  console.log('НЕТ');
-  } // задание 1
-  let a = 10
-  var e = "зима"
-  if(e === a){
-  console.log(true);
-  }
-  else{
-  console.log(false);
+  //let num = prompt ("сумма a+b")
+ // if(num > 15){
+   //console.log('ДА');
+  // }else if(num <= 15){
+  //console.log('НЕТ');
+ // } // задание 1
+  //let a = 10
+  //var e = "зима"
+ // if(e === a){
+  //console.log(true);
+ // }
+  //else{
+  //console.log(false);
   
-  } // задание 2
+ // } // задание 2
+
+ or (let a = 1; a <51; a++) {
+   console.log(a);
+    } // задание 1
   
+   for (let a = 50; a > 24; a--) {
+     console.log(a);
+       } // задание 2
+   for (let a = 2501; a < 3000; a+=2) {
+      console.log(a);
+     } // задание 3
