cc9e5ed6c496577f9767e32e4859fa973e5d26a2
Julia190287 <mus19.02.87@mail.ru>
2024-01-23 21:53:07 +0300

    homework 8

--- 1.js
+++ 1.js
@@ -38,61 +38,61 @@
   //    console.log(a);
   //   } // задание 3
 
-  let number = prompt("Введи число от 1 до 5");
-  let a = 3;
-switch (number) {
-  case '3':
-  case "3":
-    alert( 'Да' );
-    break;
+  //let number = prompt("Введи число от 1 до 5");
+  //let a = 3;
+//switch (number) {
+ // case '3':
+ // case "3":
+  //  alert( 'Да' );
+  //  break;
 
-  case "1":
-   alert( 'Нет' );
-   break;
-  case "2":
-   alert( 'Нет' );
-    break;
-  case "4":
-    alert( 'Нет' );
-   break;
- case "5":
-   alert( 'Нет' );
-    break;
+ // case "1":
+  // alert( 'Нет' );
+ //  break;
+ // case "2":
+  // alert( 'Нет' );
+  //  break;
+ // case "4":
+ //   alert( 'Нет' );
+  // break;
+ //case "5":
+ //  alert( 'Нет' );
+  //  break;
 
-  default:
-   alert( 'Не сработало' );
-} // Задание 1
+ // default:
+  // alert( 'Не сработало' );
+//} // Задание 1
 
-let table = prompt("Введи число от 1 до 12");
+//let table = prompt("Введи число от 1 до 12");
 
-switch (table) {
-  case "1":
- case "2":
- case "3":
-   alert( 'зима' );
-    break;
+//switch (table) {
+ // case "1":
+ //case "2":
+ //case "3":
+ //  alert( 'зима' );
+  //  break;
 
-  case "4":
-  case "5":
-  case "6": 
-    alert( "весна" );
-    break;
-case "7":
-  case "8":
-  case "9":  
-   alert( 'лето' );
-  break;
-  case "10":
-  case "11":
-  case "12":
-   alert( 'осень' );
-    break;
+ // case "4":
+ // case "5":
+ // case "6": 
+  //  alert( "весна" );
+  //  break;
+//case "7":
+ // case "8":
+ // case "9":  
+ //  alert( 'лето' );
+ // break;
+ // case "10":
+ // case "11":
+ // case "12":
+ //  alert( 'осень' );
+ //   break;
 
-  default:
-   alert( 'ошибка' );
-} // Задание 2
+ // default:
+  // alert( 'ошибка' );
+//} // Задание 2
 
-let digit = 1;
-let user = null
-let word = "привет"
-console.log(digit ?? user ?? word ?? "аноним"); //Задание 3
+//let digit = 1;
+//let user = null
+//let word = "привет"
+//console.log(digit ?? user ?? word ?? "аноним"); //Задание 3
